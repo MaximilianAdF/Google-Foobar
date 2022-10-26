705e40298a60efc2c0b069ad01c76096df6041f5
Maximilian Alvim de Faria <max@alvimdefaria.com>
2022-10-26 21:06:39 +0200

    Update Revised.py
    Started coding the intersect point checker, and some other things. Need to find a way to see 2nd hit and it's slope

--- Level 4/Bringing A Gun To A Trainer Fight/Revised.py
+++ Level 4/Bringing A Gun To A Trainer Fight/Revised.py
@@ -25,6 +25,8 @@ def possible_bearings(dimensions):
     return bearings
 bearings = possible_bearings([3,2])
 
+#print(bearings)
+"""
 def steps(bearings, your_position):
     x0 = your_position[0]
     y0 = your_position[1]
@@ -36,6 +38,68 @@ def steps(bearings, your_position):
         steps.append([x,y])
     return steps
 print(steps(bearings, [1,1]))
+"""
+def inclination(bearing):
+    x = bearing[0]
+    y = bearing[1]
 
-def intersect():
+    k = y/x #slope
+
+    return k
+
+
+def path(bearings, your_position):
+    paths = []
+    for i in bearings:
+        if i[0] != 0 and i[1] != 0 and [i[0],i[1]] != your_position:  #If not same position or constant
+            slope = [i[0],i[1],inclination([i[0],i[1]])]
+            intersect(slope,[1,1],[3,2])
+
+        else:
+            pass
+            #deal with constants.
+
+def intersect(slope, your_position, dimensions):
+    x = slope[0]
+    y = slope[1]
+    
+    if x < 0 and y > 0:
+        # y = kx + m
+        # m = y - kx [1,1] => m = 1 - k
+        m = 1 - slope[2] #(=y) when x = 0 (When line is hit)
+        x = your_position[0] #steps to the left required to hit x = 0
+
+        if m < dimensions[1]:
+            #Hit on left wall (x = 0) at (0,m)
+            hit = [0,m]
+        
+        elif m > dimensions[1]:
+            #Hit on top wall (y = dimensions[1])
+            # dimensions[1] = kx + m => x = (dimensions[1] - m)/k
+            hit = [(dimensions[1] - m)/slope[2], dimensions[1]]
+    
+        elif m == dimensions[1]: #Corner hit
+            hit = [0,dimensions[1]]
+
+        #Second hit???
+        print(hit)
+        
+
+
+
+    elif x < 0 and y < 0:
+        #print(slope, "+")
+        pass
+    elif x > 0 and y > 0:
+        #print(slope, "-")
+        pass
+    elif x > 0 and y < 0:
+        #print(slope, "/")
+        pass
+
+path(bearings, [1,1])
+
+
+
+def checkHit(your_position, trainer_position, distance):
     pass
